09662d2d5e07acfdc5063a3d673211fd5446ed5b
Shashank Tiwari <shashankktiwarii3@gmail.com>
2026-07-11 13:41:02 +0530

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -524,9 +524,13 @@ class DetectionModel(BaseModel):
         y[-1] = y[-1][..., i:]  # small
         return y
 
+    # def init_criterion(self):
+    #     """Initialize the loss criterion for the DetectionModel."""
+    #     return E2ELoss(self) if getattr(self, "end2end", False) else v8DetectionLoss(self)
     def init_criterion(self):
         """Initialize the loss criterion for the DetectionModel."""
-        return E2ELoss(self) if getattr(self, "end2end", False) else v8DetectionLoss(self)
+        return DensityAwareE2ELoss(self) if getattr(self, "end2end", False) else v8DetectionLoss(self)
+
 
 
 class OBBModel(DetectionModel):
